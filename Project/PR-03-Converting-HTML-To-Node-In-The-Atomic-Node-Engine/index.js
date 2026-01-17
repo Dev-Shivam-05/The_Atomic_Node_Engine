3ad95da666d2b85761fc88b99a5760e6895cb4bf
Dev-Shivam-05 <shivambhadoriya1605@gmail.com>
2026-01-17 12:00:00 +0530

    Modified index.js And  render.yaml for handling Rendering Process In PR-03-Converting-HTML-To-Node-The-Atomic-Node-Engine In The_Atomic_Node_Engine

--- Project/PR-03-Converting-HTML-To-Node-In-The-Atomic-Node-Engine/index.js
+++ Project/PR-03-Converting-HTML-To-Node-In-The-Atomic-Node-Engine/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import env from "./config/dotenv.js";
 
 const app = express();
-const port = env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
