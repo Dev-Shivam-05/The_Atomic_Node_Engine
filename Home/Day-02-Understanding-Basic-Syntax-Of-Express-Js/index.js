6b5d1f5e7b03c46cdfc8f1b40c4e6acc2a4eeca8
Dev-Shivam-05 <shivambhadoriya1605@gmail.com>
2026-03-19 12:00:00 +0530

    Initialize Day-02-Understanding-Basic-Syntax-Of-Express-Js in Home in The_Atomic_Node_Engine

---
+++ Home/Day-02-Understanding-Basic-Syntax-Of-Express-Js/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('App listening on port PORT!');
+    console.log(`http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
